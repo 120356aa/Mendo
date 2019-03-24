d14f2faf7fb745ab172990457a34faebd81901c7
120356aa <yurlovandrew@gmail.com>
2019-03-24 16:26:48 -0700

    Created header component and added reset-css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Header from './Components/Header.js';
+
+import 'reset-css';
+import { Wrap } from './Styles/GlobalStyles.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <Wrap>
+        <Header>
+
+        </Header>
+      </Wrap>
     );
   }
 }
--- src/Styles/HeaderStyles.js
+++ src/Styles/HeaderStyles.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const HeaderWrap = styled.div`
   width: 100%;
-  border: 1px solid red;
+  border: 1px solid blue;
 `;
