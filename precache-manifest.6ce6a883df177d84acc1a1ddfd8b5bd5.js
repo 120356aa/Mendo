d8b4e241f350bd9e5b54dcf78cbe72f95b3ccf25
120356aa <yurlovandrew@gmail.com>
2019-03-24 17:15:09 -0700

    Updates

---
+++ precache-manifest.6ce6a883df177d84acc1a1ddfd8b5bd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "765f4f57ccec03c91053",
+    "url": "/120356aa/Mendo/static/js/main.bd8e9e57.chunk.js"
+  },
+  {
+    "revision": "a0ff385588861cedda01",
+    "url": "/120356aa/Mendo/static/js/runtime~main.853372b9.js"
+  },
+  {
+    "revision": "a9c060adf4297f2d4423",
+    "url": "/120356aa/Mendo/static/css/2.3a52990e.chunk.css"
+  },
+  {
+    "revision": "a9c060adf4297f2d4423",
+    "url": "/120356aa/Mendo/static/js/2.d9945297.chunk.js"
+  },
+  {
+    "revision": "7a4cb89d82d3198cb01a945ad7dc7288",
+    "url": "/120356aa/Mendo/index.html"
+  }
+];
