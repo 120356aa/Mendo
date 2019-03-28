31ec5266aa7fbd28e44a6be63d9a7e338a5093ee
120356aa <yurlovandrew@gmail.com>
2019-03-28 13:23:37 -0700

    font updates

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -2,16 +2,16 @@ import styled from 'styled-components';
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-  body {
-    @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600');
-    font-family: 'Open Sans', sans-serif;
+  // body {
+  //   @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600');
+  //   font-family: 'Open Sans', sans-serif;
 
-    @import url('https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i');
-    font-family: 'Montserrat', sans-serif;
+  //   @import url('https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i');
+  //   font-family: 'Montserrat', sans-serif;
 
-    @import url('https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:900');
-    font-family: 'Alegreya Sans SC', sans-serif;
-  }
+  //   @import url('https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:900');
+  //   font-family: 'Alegreya Sans SC', sans-serif;
+  // }
 `;
 
 export const Wrap = styled.div`
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { HashRouter } from 'react-router-dom';
 
-
 ReactDOM.render(
 <Router basename="/Mendo">
   <App />
